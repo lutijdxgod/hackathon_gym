aa5b9dcc2ad4ae2d76e0c8de990084575ee01a69
Daniil Novikov <den31804@mail.ru>
2024-10-21 02:51:25 +0300

    added function that converts all id fields(including those that end with _id) to string

--- app/api/functions/funcs.py
+++ app/api/functions/funcs.py
@@ -20,3 +20,35 @@ async def get_entity_by_field(
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
     return result
+
+
+async def get_entity_by_field_nullable(
+    entity: Table,
+    field: InstrumentedAttribute,
+    value: Any,
+    session: AsyncSession,
+):
+    query = select(entity).where(field == value)
+    query_result = await session.scalars(query)
+    result = query_result.all()
+
+    return result
+
+
+def db_row_ids_to_str(element):
+    # TODO replace __dict__ because it contains unnecessary entries
+    element_dict: dict[str, Any] = element.__dict__
+    for attr, val in element_dict.items():
+        if attr.endswith("_id") or attr == "id":
+            element_dict[attr] = str(val)
+    return element_dict
+
+
+def ids_to_string(elements):
+    if isinstance(elements, list):
+        processed_elements = []
+        for i in range(len(elements)):
+            processed_elements.append(db_row_ids_to_str(element=elements[i]))
+        return processed_elements
+    else:
+        return db_row_ids_to_str(element=elements)
