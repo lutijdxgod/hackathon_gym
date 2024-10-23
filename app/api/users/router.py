a27b1d73f1a04e3d8ad44d5cc2ebd7003c3428b3
Daniil Novikov <den31804@mail.ru>
2024-10-24 01:21:09 +0300

    changed response model of /profile

--- app/api/users/router.py
+++ app/api/users/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy import select
-from sqlalchemy.orm import selectinload
+from sqlalchemy.orm import joinedload
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.api.functions.funcs import get_entity_by_field_nullable, ids_to_string
 from app.models.models import Subscription, User, UserInfo
@@ -22,27 +22,13 @@ async def get_users_subscriptions(
     return ids_to_string(subscriptions)
 
 
-@router.get("/profile", response_model=list[ProfileUser])
+@router.get("/profile", response_model=ProfileUser)
 async def get_users_profile(db: AsyncSession = Depends(db.session_getter), user: UserOut = Depends(get_current_user)):
-    # profile_query = select(User, UserInfo).join(UserInfo, UserInfo.user_id == User.id)
-    # print(profile_query.compile(compile_kwargs={"literal_bind": True}))
-    # query_result = await db.execute(profile_query)
-    # profile = query_result.all()
-    # print(f"{profile=}")
-    # profile_info = []
-    # for user, user_info in profile:
-    #     info_to_return = user.__dict__
-    #     info_to_return["user_info"] = user_info.__dict__
-    #     profile_info.append(info_to_return)
-
-    profile_query = select(User).options(selectinload(User.user_info))
+    profile_query = select(User).options(joinedload(User.user_info))
     query_result = await db.execute(profile_query)
-    profile: list[User] = query_result.scalars().all()
-    print(f"{profile=} {profile[0].user_info}")
-    profile_info = []
-    for user in profile:
-        info_to_return = user.__dict__
-        info_to_return["user_info"] = user.user_info.__dict__
-        profile_info.append(info_to_return)
+    profile: list[User] = query_result.scalars().first()
+    print(f"{profile=}")
+    info_to_return = user.__dict__
+    info_to_return["user_info"] = user.user_info.__dict__
 
-    return profile_info
+    return info_to_return
